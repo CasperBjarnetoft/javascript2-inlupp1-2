5fa46182b0a7e62d95381def4c7735b583b5ec3d
Casper Bjarnetoft <casper.ason@hotmail.se>
2022-03-30 09:58:37 +0200

    lagt till kod

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 const cartModel = require('../models/shoppingcart/cartModel')
-const auth = require('../authentication/auth');
 
 // POST (Create a product)
-router.post('/', auth.verifyToken, cartModel.createCart)
+router.post('/', cartModel.createCart)
 
 
 module.exports = router;
